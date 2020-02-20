b8bda01974e1b4e09df4874d84ae702fc9040006
TarCode <tarcode33@gmail.com>
2020-02-20 21:30:52 +0200

    updated fields fetched

--- main.py
+++ main.py
@@ -26,32 +26,39 @@ def get_listings_per_page(url, number_of_pages):
             price = listing.find(class_='p24_price')
             title = listing.find(class_='p24_title')
             location = listing.find(class_='p24_location')
-            description = listing.find(class_='p24_excerpt')
+            bathrooms = listing.find(class_='p24_featureDetails', attrs={"title":'Bathrooms'})
+            parking = listing.find(class_='p24_featureDetails', attrs={"title":'Parking Spaces'})
             area = listing.find(class_='p24_size')
-            if title and location and price and area and description:
+
+            if title and location and price and area and bathrooms and parking:
                 title = title.text.strip()
                 location = location.text.strip()
                 price = price.text.strip()
                 area = area.text.strip()
+                bathrooms = bathrooms.text.strip()
+                parking = parking.text.strip()
+
                 if "house" in title.lower() or "apartment" in title.lower():
                     processed_listings.append({
                         "bedrooms":  float(re.search(r'\d+', title).group()) if "bedroom" in title.lower() else 0,
                         "location": location,
                         "area": float(re.search(r'\d+', area).group()),
+                        "bathrooms": bathrooms,
+                        "parking": parking,
                         "price":  float(re.search(r'\d+', unidecode(price).replace(" ", "")).group()) if price != 'POA' else 0
                     })
 
     return processed_listings
 
 
-URL = "https://www.property24.com/apartments-for-sale/observatory/cape-town/western-cape/10157"
+URL = "https://www.property24.com/apartments-for-sale/gardens/cape-town/western-cape/9145"
 
-processed_listing_data = get_listings_per_page(URL, 8)
+processed_listing_data = get_listings_per_page(URL, 9)
 
 print(processed_listing_data)
 
 with open('property-listing.csv', mode='w') as listings_file:
-    fieldnames = ['bedrooms', 'location', 'area', 'price']
+    fieldnames = ['bedrooms', 'location', 'area', 'bathrooms', 'parking', 'price']
     writer = csv.DictWriter(listings_file, delimiter=',', quotechar='"', fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL)
     writer.writeheader()
     for prop_listing in processed_listing_data:
